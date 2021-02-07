72bb06417b59016bef0e49900da2b6b366ee09bf
kaan1997-art <77634025+kaan1997-art@users.noreply.github.com>
2021-02-07 18:46:20 +0300

    Add files via upload

---
+++ Palindrom1/Palindrom1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir kelime giriniz: ");
+            string kelime = Console.ReadLine();
+            string kelimetersi = "";
+            while (kelime != "q")
+            {
+                if (kelime.Length >= 2)
+                {
+                    for (int i = kelime.Length - 1; i >= 0; i--)
+                    {
+                        kelimetersi += kelime[i];
+
+                    }
+                    Console.WriteLine("\nKelimenin tersten yazılısı = " + kelimetersi);
+                    if (kelime == kelimetersi)
+                    {
+                        Console.WriteLine("\nGirilen kelime Palindrom bir kelimedir.");
+                    }
+                    else //if (kelimetersi != kelime)
+                    {
+                        Console.WriteLine("\nGirilen kelime Palindrom bir kelime değildir.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("İki harften az giremezsiniz!");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
